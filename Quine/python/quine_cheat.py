056248add695703dd58d09d5fc2027f70f90fe29
lezhangisu <lezhangisu@gmail.com>
2018-08-24 15:36:19 -0500

    categorize

---
+++ Quine/python/quine_cheat.py
@@ -0,0 +1,2 @@
+# This is a cheat version of quine
+print open(__file__).read()
---
+++ Quine/python/quine_short.py
@@ -0,0 +1,2 @@
+#This is a program which prints itself
+s='#This is a program which prints itself\ns=%r;print(s%%s)';print(s%s)
