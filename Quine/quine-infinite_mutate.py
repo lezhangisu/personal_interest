ca937974f628055179b2e64314d8b3e6d6081a4e
lezhangisu <lezhangisu@gmail.com>
2018-08-24 15:30:35 -0500

    initial

---
+++ Quine/quine-infinite_mutate.py
@@ -0,0 +1,2 @@
+# DO NOT run this on any working machine. It will eat up ALL your memory. Run it with VMs.
+import os;import random;fn="".join([str(rand) for rand in random.sample(xrange(100), 10)]);s='import os;import random;fn="".join([str(rand) for rand in random.sample(xrange(100), 10)]);s=%r;open(fn+".py", "w").write(s%%s);print(s%%s);os.system("python "+fn+".py")';open(fn+".py", "w").write(s%s);print(s%s);os.system("python "+fn+".py")
